83b7eb56a76ce1f734f7df3ca83f0b5c4952be59
Oscar Julián <oscarjp97@hotmail.com>
2023-09-21 13:20:26 +0200

    Update main.m

--- P3.2_LS_ACOUSTICS/main.m
+++ P3.2_LS_ACOUSTICS/main.m
@@ -1,6 +1,6 @@
 %% PRACTICA 3
-% Oscar Julián Ponte
-% Oriol Guimó Morell
+% Oscar JuliÃ¡n Ponte
+% Oriol GuimÃ³ Morell
 %Grup Classe: D & E
 %Versi? MATLAB: R2019b
 %Data: 04/04/2020
@@ -71,27 +71,3 @@ disp(area2)
 
 disp('Area de la figura Omnidireccional:')
 disp(area3)
-
-%% PREGUNTES ----------------------------------------------
-
-% 1.
-% El mejor patrón que podemos utilizar para que se escuche en la pista
-% sin molestar a los residentes es el cardioide (o supercardioide) ya que
-% las ondas de sonido se propagan únicamente hacia delante, con un pequeño 
-% residuo de sonido que va hacia atrás.
-% Con el omnidireccional molestaríamos demasiado a los residentes i con el
-% bidireccional, no les molestaríamos, pero tampoco se oiría nada en la
-% pista.
-
-% 2.
-% Bajo nuestro punto de vista, creemos que si que sería más preciso 
-% calcularlo con el método de los rectángulos ya que ese método es muy 
-% preciso y calcula toda el área sin posibilidad de dejarse nada.
-% (Riemann)
-
-% 3.
-% Area teórica = 28,2743
-% Area Matlab (h=pi/10) = 70,5858
-% Area Matlab (h=pi/1000) = 0,7069
-% Hemos encontrado que con la resolución de h=(0.2*pi/5) és la que más se
-% aproxima. 
